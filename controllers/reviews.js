f534ad87f25f4e9ae8523f476e78678d106c4793
KeyNun95 <k.nunez2017@gmail.com>
2023-07-12 16:02:43 -0400

    review create done

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,1 +1,27 @@
-const BookModel = require("../models/book");
+const BookModel = require('../models/book');
+
+module.exports = {
+    create
+};
+
+async function create(req, res) {
+    try {
+        const booksListed = await BookModel.findById(req.params.id)
+        req.body.user = req.user._id;
+        req.body.userName = req.user.name;
+        booksListed.reviews.push(req.body);
+        await booksListed.save();
+        console.log(booksListed);
+        res.redirect(`/movies/${req.params.id}`);
+    } catch(err) {
+        res.send(err);
+    }
+}
+
+async function deleteReview(req, res, next) {
+    try {
+        const bookReview = await BookModel.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id});
+        if(!bookReview) return res.redirect('/books')
+        bookReview.reviews.remove(req.params.id);
+    }
+}
